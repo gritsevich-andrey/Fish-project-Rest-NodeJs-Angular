511ac6e74d04df1655511247bd90692462013784
gritsevich-andrey <gritsevich-andrey@yandex.ru>
2021-09-28 21:22:56 +0300

    Add morgan and corsJs

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
-const authRoutes = require('./routes/auth')
+const authRoutes = require('./routes/auth');
+const bodyParser = require('body-parser');
 
+app.use(require('morgan')('dev'));
+app.use(require('cors')());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 
 module.exports = app;
