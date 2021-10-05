01654f06a168ebb8cbd70abaac551dc60442070e
gritsevich-andrey <gritsevich-andrey@yandex.ru>
2021-10-05 19:07:41 +0300

    Add form login and register

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { RegisterComponent } from './register/register.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AuthLayoutComponent,
+    SiteLayoutComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  form!: FormGroup;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(' ', [Validators.required, Validators.email]),
+      password: new FormControl(' ', [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  onSubmit() {
+
+  }
+  get f()
+  {
+    return this.form.controls;
+  }
+}
