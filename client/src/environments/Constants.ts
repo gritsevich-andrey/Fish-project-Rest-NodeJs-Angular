bde616ef884187554e076cba103e3e44d34be47a
gritsevich-andrey <gritsevich-andrey@yandex.ru>
2021-10-28 20:08:01 +0300

    Add photo in cabinet

---
+++ client/src/environments/Constants.ts
@@ -0,0 +1,8 @@
+export interface Constants {
+  production: boolean;
+  AUTH_API: string;
+  CABINET_API: string;
+  ADMIN_API: string;
+  COMPLAINT_API: string;
+  PHOTO_API: string;
+}
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,10 +2,15 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+import {Constants} from "./Constants";
+export const environment: Constants = {
   production: false,
-  backUrl: 'http://localhost:5000/'
-};
+  AUTH_API: '/api/auth',
+  CABINET_API: '/api/cabinet',
+  ADMIN_API: '/api/administrator',
+  COMPLAINT_API: '/api/complaint',
+  PHOTO_API: '/api/photo'
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
