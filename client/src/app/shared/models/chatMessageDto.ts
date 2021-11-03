9cbe643d152a842a755f724d40ff141d8e100842
pavel-coder <pavelpliyiko2000@gmail.com>
2021-11-03 14:13:41 +0300

    started work on feed

--- client/src/app/shared/models/chatMessageDto.ts
+++ client/src/app/shared/models/chatMessageDto.ts
@@ -1,9 +1,11 @@
 export class ChatMessageDto {
   user: string;
   message: string;
+  file: string;
 
-  constructor(user: string, message: string){
+  constructor(user: string, message: string, file: string){
     this.user = user;
     this.message = message;
+    this.file = file
   }
 }
--- client/src/app/site-pages/live-feed/live-feed.component.ts
+++ client/src/app/site-pages/live-feed/live-feed.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {WebsocketService} from "../../shared/services/websocket.service";
 import {ChatMessageDto} from "../../shared/models/chatMessageDto";
-import {NgForm} from "@angular/forms";
+import {FormArray, FormControl, FormGroup, NgForm, Validators} from "@angular/forms";
 import {UserService} from "../../shared/services/user.service";
+import {mimeType} from "../cabinet/mime-type.validator";
 
 @Component({
   selector: 'app-live-feed',
@@ -10,21 +11,43 @@ import {UserService} from "../../shared/services/user.service";
   styleUrls: ['./live-feed.component.scss']
 })
 export class LiveFeedComponent implements OnInit, OnDestroy {
-userEmail!: string;
+  userEmail!: string;
+  file!: File;
+  form!: FormGroup;
+  imagePreview = 'uploads/file.jpg';
+
   constructor(public webSocketService: WebsocketService,
               private userService: UserService) {
+    this.form = new FormGroup({
+      description:  new FormControl(''),
+      file: new FormControl('')
+    });
   }
 
   ngOnInit(): void {
     this.webSocketService.openWebSocket();
-    this.userEmail= this.userService.getUserDataFromLocal();
+    this.userEmail = this.userService.getUserDataFromLocal();
     console.log(this.userEmail);
   }
-  sendMessage(sendForm: NgForm) {
-    const chatMessageDto = new ChatMessageDto(sendForm.value.user, sendForm.value.message);
+
+  sendMessage() {
+    const chatMessageDto = new ChatMessageDto(this.userEmail, this.form.value.description, this.form.value.file);
     this.webSocketService.sendMessage(chatMessageDto);
-    sendForm.controls.message.reset();
+    this.form.controls.description.reset();
+    this.form.controls.file.reset();
   }
+
+  onImageLoad(event: Event) {
+    // @ts-ignore
+    const file = (event.target as HTMLInputElement).files[0];
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.form.patchValue({file: reader.result});
+    }
+    reader.readAsDataURL(file);
+  }
+
   ngOnDestroy(): void {
     this.webSocketService.closeWebSocket();
   }
