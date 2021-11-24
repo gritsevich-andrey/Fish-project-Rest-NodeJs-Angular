624b2201a1a6e13e59f6022193fe22470050217d
gritsevich-andrey <gritsevich-andrey@yandex.ru>
2021-11-24 19:16:28 +0300

    Add dialog window for map

--- client/src/app/site-pages/map-travel/list-descriptions/list-descriptions.component.ts
+++ client/src/app/site-pages/map-travel/list-descriptions/list-descriptions.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Travel} from "../../../shared/interfaces";
+import {MatDialog} from "@angular/material/dialog";
+import {DialogComponent} from "./dialog/dialog.component";
 
 @Component({
   selector: 'app-list-descriptions',
@@ -9,10 +11,20 @@ import {Travel} from "../../../shared/interfaces";
 export class ListDescriptionsComponent implements OnInit {
   //@ts-ignore
 @Input() travels: Travel;
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
     console.log(this.travels);
   }
 
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent,
+    //   {
+    //   width: '250px'
+    // }
+    );
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
