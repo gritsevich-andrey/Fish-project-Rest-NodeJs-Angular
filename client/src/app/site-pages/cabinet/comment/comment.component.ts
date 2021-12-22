78f8bf5a80e88c1f74e4a8cbd3a0b99e9960af19
gritsevich-andrey <gritsevich-andrey@yandex.ru>
2021-12-22 19:13:23 +0300

    Add general ratings

--- client/src/app/site-pages/cabinet/comment/comment.component.ts
+++ client/src/app/site-pages/cabinet/comment/comment.component.ts
@@ -8,11 +8,23 @@ import {Component, Input, OnInit} from '@angular/core';
 export class CommentComponent implements OnInit {
 @Input() reviews: any[]=[];
   @Input() ratings: any[]=[];
+  @Input() sumRating: number = 0;
   valueRadio = '';
-  constructor() { }
-
-  ngOnInit(): void {
-  console.log('Что принимает компонент отзывы', this.reviews, this.ratings);
+  idS = '100';
+  constructor() {
+    console.log('Объект рейтинга', this.ratings);
   }
 
+  ngOnInit(): void {
+    const ratings = this.ratings;
+    console.log('сумма рейтинга', ratings);
+  // let sum =  this.ratings.reduce((previousValue, currentValue) => {
+  //     return previousValue + currentValue/2;
+  //   }, 0);
+  // console.log('сумма рейтинга', sum);
+  }
+
+  sumRatings(event: any) {
+    console.log('Эвент', event);
+  }
 }
