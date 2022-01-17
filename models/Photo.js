e29e7ccf33be6d7202810e1a7cf48887f39adf2c
gritsevich-andrey <gritsevich-andrey@yandex.ru>
2022-01-17 16:49:52 +0300

    Add field queryDeleted in photo

--- models/Photo.js
+++ models/Photo.js
@@ -56,6 +56,10 @@ const photoSchema = new Schema({
             type: String,
             required: true
         }
-    }]
+    }],
+    queryDeleted: {
+        type: Boolean,
+        default: false
+    },
 });
 module.exports = mongoose.model('photos', photoSchema);
