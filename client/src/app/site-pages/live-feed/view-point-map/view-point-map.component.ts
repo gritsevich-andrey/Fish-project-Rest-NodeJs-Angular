ae99b7351369e475bad751b3d59790c879eb8e62
pavel-coder <pavelpliyiko2000@gmail.com>
2022-01-18 17:57:11 +0300

    delete fullsceen map in select point and view point

--- client/src/app/site-pages/live-feed/view-point-map/view-point-map.component.ts
+++ client/src/app/site-pages/live-feed/view-point-map/view-point-map.component.ts
@@ -1,5 +1,6 @@
 import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 import {Component, Inject, OnInit} from '@angular/core';
+import {YaReadyEvent} from "angular8-yandex-maps";
 
 @Component({
   selector: 'app-view-point-map',
@@ -14,5 +15,10 @@ export class ViewPointMapComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onMapReady(event: YaReadyEvent<ymaps.Map>) {
+    let map = event.target;
+    map.controls.remove('fullscreenControl')
   }
 }
